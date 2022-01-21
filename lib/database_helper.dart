8d239bd832f3f9843aeabe2a0bd0619e6b324b48
VershimaKelvin <kelvinityavyar@gmail.com>
2022-01-21 13:25:06 +0100

    completed the creation of the database

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,28 +1,48 @@
 import 'dart:io';
-
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper{
 
 
+
+  static const _dbName = 'myDatabase.db';
+  static const _dbVersion = 1;
+  static const _columnName = "name";
+  static const _columnID = 'ID';
+  static const _tableName = 'myTable';
+
   //making DatabaseHelper a Singleton class
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
-  static Database _database;
+  static Database? _database;
 
   Future<Database> get database async{
     if(_database != null){
-      return _database;
+      return _database!;
     }
     _database = await _initiateDatabase();
-    return _database;
+    return _database!;
   }
 
   _initiateDatabase()async{
     Directory directory = await getApplicationDocumentsDirectory();
-    //String path = join();
+    String path = join(directory.path, _dbName);
+    await openDatabase(path,version: _dbVersion,onCreate: onCreate);
+
+  }
+
+  onCreate(Database db,int version) {
+    db.query(
+        '''
+        CREATE TABLE $_tableName(
+        $_columnID PRIMARY KEY,
+        $_columnName TEXT NOT NULL
+        )
+        '''
+    ) ;
 
   }
 
